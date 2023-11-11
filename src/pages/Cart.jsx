d2102e11cb37e13d3fcf9ec57b2783d5e9df26df
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-11 15:18:50 +0530

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,19 +1,43 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../redux/cartSlice";
 
 const Cart = () => {
 	const itemsDetails = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
+
+	const total = (itemsDetails) => {
+		return itemsDetails.reduce(
+			(accumulator, item) => accumulator + item.price,
+			0
+		);
+	};
+
+	const handleRemove = (itemId) => {
+		dispatch(remove(itemId));
+	};
+
 	return (
-		<div className='productsWrapper'>
-			{itemsDetails.map((items) => (
-				<div className='card' key={items.id}>
-					<img src={items.image} alt='' />
-					<h4>{items.title}</h4>
-					<h5>Price: ${items.price}</h5>
-				</div>
-			))}
-		</div>
+		<>
+			<h3 className='total'>Total : ${total(itemsDetails)} </h3>
+			<div className='productsWrapper'>
+				{itemsDetails.map((item) => (
+					<div className='card' key={item.id}>
+						<img src={item.image} alt='' />
+						<h4>{item.title}</h4>
+						<h5>Price: ${item.price}</h5>
+						<button
+							className='btn'
+							onClick={() => {
+								handleRemove(item.id);
+							}}>
+							Remove from cart
+						</button>
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
