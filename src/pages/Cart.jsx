1a8b81febf6d68ee488863c33b182268d3b78de5
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-11 13:46:35 +0530

    added cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-	return <div>Cart</div>;
+	const itemsDetails = useSelector((state) => state.cart);
+	return (
+		<div className='productsWrapper'>
+			{itemsDetails.map((items) => (
+				<div className='card' key={items.id}>
+					<img src={items.image} alt='' />
+					<h4>{items.title}</h4>
+					<h5>Price: ${items.price}</h5>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Cart;
